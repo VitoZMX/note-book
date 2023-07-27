9384b7b27e106d41122ff5caa333e290dbec529f
VitoZMX <Vi-69@yandex.ru>
2023-07-27 18:33:37 +0300

    feat: add actionTypes

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,9 @@
+export const SET_ALL_NOTES = 'SET_ALL_NOTES'
+export const ADD_NOTE = 'ADD_NOTE'
+export const ADD_EDIT_NOTE = 'ADD_EDIT_NOTE'
+export const REMOVE_NOTE = 'REMOVE_NOTE'
+
+export const ADD_TAG = 'ADD_TAG'
+
+export const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS'
+export const SET_THEME = 'SET_THEME'
