b46399a20bc44f777c93c2088296964a8837b219
VitoZMX <Vi-69@yandex.ru>
2023-08-07 17:14:39 +0300

    feat: added sorting by date

--- src/api/notes-api.ts
+++ src/api/notes-api.ts
@@ -1,5 +1,16 @@
-import {addDoc, collection, deleteDoc, getDocs, query, updateDoc, where} from 'firebase/firestore'
-import {INoteType} from '../types/types'
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    getDoc,
+    getDocs,
+    orderBy,
+    query,
+    serverTimestamp,
+    updateDoc,
+    where
+} from 'firebase/firestore'
+import {INoteType, NoteDateType} from '../types/types'
 import {dataBase} from './firebaseConfig'
 
 const pathCollectionNotes = 'notes'
@@ -7,20 +18,27 @@ const CollectionNotes = collection(dataBase, pathCollectionNotes)
 
 export const notesAPI = {
     async getAllNotes() {
-        const querySnapshot = await getDocs(CollectionNotes)
+        const querySnapshot = await getDocs(query(CollectionNotes, orderBy('date', 'desc')))
         return querySnapshot.docs.map((doc) => ({
             ...doc.data(),
         })) as INoteType[]
     },
     async setNote(note: INoteType) {
-        await addDoc(CollectionNotes, note)
+        const noteWithDate = {
+            ...note,
+            date: serverTimestamp(),
+        }
+        const docRef = await addDoc(CollectionNotes, noteWithDate)
+        const docSnapshot = await getDoc(docRef)
+        const {date} = docSnapshot.data() as { date: NoteDateType }
+        return date
     },
     async setEditedNote(note: INoteType) {
         const querySEN = query(CollectionNotes, where('id', '==', note.id))
         const querySnapshot = await getDocs(querySEN)
         const docSnapshot = querySnapshot.docs[0]
         if (docSnapshot) {
-            return await updateDoc(docSnapshot.ref, {tag: note.tag, text: note.text})
+            return await updateDoc(docSnapshot.ref, {tags: note.tags, text: note.text})
         }
     },
     async delNote(noteID: string) {
