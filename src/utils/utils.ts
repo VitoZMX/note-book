37bb3f52cd3d19800d1006083d647c85462238fa
VitoZMX <Vi-69@yandex.ru>
2023-08-07 17:20:37 +0300

    feat: add func to convert seconds to date & refactor func parseNoteData

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,18 +3,35 @@ import {v1} from 'uuid'
 
 export function parseNoteData(noteText: string, OldId?: string): INoteType {
     let id = OldId
-    const index = noteText.indexOf('#')
-    const text = index === -1 ? noteText.trim() : noteText.substring(0, index).trim()
-    const tag = index === -1 ? '' : noteText.substring(index).trim()
+    const parts = noteText.split('#').map(part => part.trim())
+
+    const text = parts[0]
+    const tags = parts.slice(1).map(tag => `#${tag}`)
+
     if (!id) {
         id = v1()
     }
-    return {text, tag, id}
+    return {text, tags, id, date: {seconds: 0, nanoseconds: 0}}
 }
 
 export function filterNotesByTags(notes: INoteType[], tags: string[]): INoteType[] {
     if (tags.length === 0) return notes
-    return notes.filter(note => tags.includes(note.tag))
+    return notes.filter(note => note.tags.some(tag => tags.includes(tag)))
+}
+
+export function convertSecondsToDateTime(seconds: number): string {
+    if (!seconds) return ''
+    const milliseconds = seconds * 1000
+    const date = new Date(milliseconds)
+
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    }
+    return date.toLocaleString('ru-RU', options)
 }
 
 export function removeDuplicatesTags(tags: string[]): string[] {
